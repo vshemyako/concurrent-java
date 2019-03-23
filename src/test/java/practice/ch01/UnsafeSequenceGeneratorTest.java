7d6c08426c47e3b5bafb3d5cd9fca6b3a29e5865
Valentine Shemyako <v.shemyako@gmail.com>
2019-03-23 08:53:45 +0300

    Created thread safe sequence generator

--- src/test/java/practice/ch01/UnsafeSequenceGeneratorTest.java
+++ src/test/java/practice/ch01/UnsafeSequenceGeneratorTest.java
@@ -1,42 +1,60 @@
 package practice.ch01;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Verifies that {@link UnsafeSequenceGenerator} is indeed
  * implemented in non-safe thread manner.
  */
 public class UnsafeSequenceGeneratorTest {
-
-    private UnsafeSequenceGenerator sequenceGenerator;
-
-    @Before
-    public void setUp() {
-        sequenceGenerator = new UnsafeSequenceGenerator();
-    }
+    private static UnsafeSequenceGenerator unsafeSequenceGenerator = new UnsafeSequenceGenerator();
+    private static SafeSequenceGenerator safeSequenceGenerator = new SafeSequenceGenerator();
+    private static int threadsNumber = Runtime.getRuntime().availableProcessors() * 2;
 
     /**
      * Just an illustration that multiple threads get the same numbers (race condition).
      */
     @Test
     public void shouldBeUnsafe() {
-        int threadsNumber = Runtime.getRuntime().availableProcessors() * 2;
-        System.out.println("Number of executing threads: " + threadsNumber);
-
         ExecutorService executorService = null;
         try {
             executorService = Executors.newFixedThreadPool(threadsNumber);
             for (int index = 0; index < threadsNumber; index++) {
                 executorService.execute(() -> {
-                    int nextValue = sequenceGenerator.getNextValue();
+                    int nextValue = unsafeSequenceGenerator.getNextValue();
                     System.out.println(nextValue);
                 });
             }
         } finally {
+            if (executorService != null) {
+                executorService.shutdown();
+            }
+        }
+    }
+
+    /**
+     * Just an example that different threads do not run a race condition.
+     */
+    @Test
+    public static void main(String... args) {
+        ScheduledExecutorService executorService = null;
+        try {
+            executorService = Executors.newScheduledThreadPool(threadsNumber);
+            for (int index = 0; index < threadsNumber; index++) {
+                executorService.schedule(() -> {
+                    safeSequenceGenerator.getValue();
+                }, 1, TimeUnit.SECONDS);
+            }
+        } finally {
+            System.out.println("Finished");
             if (executorService != null) {
                 executorService.shutdown();
             }
