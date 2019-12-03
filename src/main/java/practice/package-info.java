00c27f16bb05b5f5c037e9ec881fce7c620e3722
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-03 23:45:14 +0300

    Added main notes from chapter 2.2

--- src/main/java/practice/package-info.java
+++ src/main/java/practice/package-info.java
@@ -12,5 +12,15 @@
  * Sub chapter 2.1<br>
  * - Thread safe classes encapsulate any needed synchronization so that clients don't have to provide their own.<br>
  * - Stateless objects are always thread safe (local variables are stored on the thread's stack)<br>
+ * <br>
+ * Sub chapter 2.2<br>
+ * - <b>Race condition</b> - check-then-act scenario when the correctness of a program relies on relative timing or
+ * interleaving of multiple threads by the runtime.<br>
+ * - <b>Data race</b> - read-modify-write scenario when synchronization is not used to coordinate access to a shared
+ * mutable state.<br>
+ * - <b>Compound action</b> - sequences of operations which have to be executed atomically in order to stay thread-safe.<br>
+ * Sub chapter 2.3<br>
+ * - When multiple variables participate in an invariant, they are no longer independent and the value of one constrains
+ * the allowed values of the others.<br>
  */
 package practice;
