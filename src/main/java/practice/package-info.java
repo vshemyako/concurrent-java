a32786941e508e291febb611fffdfb4b3591b0d6
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-03 21:17:26 +0300

    Added main notes from chapter 2.0

---
+++ src/main/java/practice/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Notes:<br>
+ * <br>
+ * Chapter 2:<br>
+ * <br>
+ * Sub chapter 2.0<br>
+ * - Thread-safe code is about access management to a shared mutable state of an object.<br>
+ * - There're three ways to make a program thread-safe:<br>
+ * -- 1. Don't share state variables across threads<br>
+ * -- 2. Make state variables immutable<br>
+ * -- 3. Use synchronization whenever accessing state variables<br>
+ */
+package practice;
