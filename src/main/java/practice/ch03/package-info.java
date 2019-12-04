6fd7b2bd0a7bb1ef9fa403eeb505df0fa44bda9d
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-04 22:40:03 +0300

    Added notes for 3.1. sub chapter

--- src/main/java/practice/ch03/package-info.java
+++ src/main/java/practice/ch03/package-info.java
@@ -3,6 +3,11 @@
  * <br>
  * Chapter 3:<br>
  * <br>
+ * Sub chapter 3.1<br>
+ * <br>
  * - In the absence of proper synchronization compiler, processor and/or runtime can reorder the operations.<br>
+ * - Locking is not only about mutual exclusion but also about memory visibility, so that all of the threads see
+ * the most up-to-date values of shared variables.<br>
+ * - Volatile variables can only guarantee visibility.<br>
  */
 package practice.ch03;
