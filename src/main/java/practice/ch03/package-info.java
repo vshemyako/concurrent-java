8b60c51f53d8aa66e9153e815c45a39cee5765c8
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-04 23:36:53 +0300

    Added notes from 3.2. chapter

--- src/main/java/practice/ch03/package-info.java
+++ src/main/java/practice/ch03/package-info.java
@@ -9,5 +9,7 @@
  * - Locking is not only about mutual exclusion but also about memory visibility, so that all of the threads see
  * the most up-to-date values of shared variables.<br>
  * - Volatile variables can only guarantee visibility.<br>
+ * Sub chapter 3.2<br>
+ * - Do not allow to escape 'this' reference during construction.<br>
  */
 package practice.ch03;
