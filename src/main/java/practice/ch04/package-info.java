5b452bc18ab1e239c5c5e53e968fbcec5b1cd697
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-07 13:03:59 +0300

    Finished chapter 4.2

--- src/main/java/practice/ch04/package-info.java
+++ src/main/java/practice/ch04/package-info.java
@@ -6,5 +6,8 @@
  * for thread safety without the need to analyze the whole program.<br>
  * - There are advantages of using private lock object instead of object's intrinsic lock, because making lock object
  * private encapsulates the lock and the client cannot acquire it.<br>
+ * Sub chapter 4.2<br>
+ * - If a class is composed of multiple independent thread-safe state variables and has no operations that have any
+ * invalid state transitions, then it can delegate thread safety to the underlying state variables.<br>
  */
 package practice.ch04;
