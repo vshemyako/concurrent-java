495c93ba7b89dcc5e4342b8bdc69a510b9409185
Valentine Shemyako <valentineshemyako@rentalcars.com>
2019-12-09 20:47:29 +0300

    Added notes for chapter 5.3 and chapter 5.4

---
+++ src/main/java/practice/ch05/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Chapter 5:<br>
+ * <br>
+ * Sub chapter 5.3<br>
+ * Bounded queues are a powerful resource management tool for building reliable applications: they make your program
+ * more robust to overload by throttling activities that threaten to produce more work than can be handled.<br>
+ * Sub chapter 5.4<br>
+ * The only situation when it is possible to ignore an interrupt is when you're controlling all of the code
+ * higher up the stack.<br>
+ **/
+package practice.ch05;
