849240ae3a3e843ffb39dc28d93d6531f48b67cf
Valentine Shemyako <valentineshemyako@rentalcars.com>
2019-12-09 22:20:47 +0300

    Added definitions for latches and semaphores

--- src/main/java/practice/ch05/package-info.java
+++ src/main/java/practice/ch05/package-info.java
@@ -7,5 +7,10 @@
  * Sub chapter 5.4<br>
  * The only situation when it is possible to ignore an interrupt is when you're controlling all of the code
  * higher up the stack.<br>
+ * Sub chapter 5.5<br>
+ * A synchronizer is any object that synchronizes the control flow of threads based on its state.<br>
+ * <b>Latch</b> is a synchronizer that can delay progress of threads until it reaches its terminal state.<br>
+ * <b>Semaphore</b> is a synchronizer that controls number of activities that can access the resource or perform a given
+ * action at the same time.<br>
  **/
 package practice.ch05;
