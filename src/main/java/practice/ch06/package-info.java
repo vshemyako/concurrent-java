717a914a074badd84e8ae0014e4355b46526affb
vshemyako <v.shemyako@gmail.com>
2019-12-10 23:35:56 +0300

    Added some notes about Executor interface

--- src/main/java/practice/ch06/package-info.java
+++ src/main/java/practice/ch06/package-info.java
@@ -5,5 +5,10 @@
  * Up to a certain point, more threads can improve throughput, but beyond that point creating more threads just slows<br>
  * down your application, and creating one thread too many can cause your entire application to crash horribly.<br>
  * Sub chapter 6.2<br>
+ * The primary abstraction for asynchronous task execution in Java class libraries is <b>not Thread</b> but Executor.<br>
+ * The easiest way to implement <b>producer - consumer</b> pattern is to use Executor.<br>
+ * When you see code of the form:<br>
+ * new Thread(runnable).start()<br>
+ * and you think you might want a more flexible execution policy, seriously consider replacing it with Executor.<br>
  **/
 package practice.ch06;
