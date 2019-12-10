16cba68c772b7c2daf529bf995330af33e7db50c
Valentine Shemyako <valentineshemyako@rentalcars.com>
2019-12-10 22:53:15 +0300

    Finished chapter 6.1

---
+++ src/main/java/practice/ch06/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Chapter 6:<br>
+ * <br>
+ * Sub chapter 6.1<br>
+ * Up to a certain point, more threads can improve throughput, but beyond that point creating more threads just slows<br>
+ * down your application, and creating one thread too many can cause your entire application to crash horribly.<br>
+ **/
+package practice.ch06;
