9a779a86cbf272fae35b3c76babbe94cfbe11e3b
vshemyako <v.shemyako@gmail.com>
2019-12-14 16:00:28 +0300

    Added 'cancellable' term

--- src/main/java/practice/ch07/package-info.java
+++ src/main/java/practice/ch07/package-info.java
@@ -2,5 +2,6 @@
  * Chapter 7:<br>
  * <br>
  * Sub chapter 7.1<br>
+ * An activity is cancellable if an external code can move it to completion before its normal completion.<br>
  **/
 package practice.ch07;
