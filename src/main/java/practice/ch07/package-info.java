f55876479ca4d2d869b96bc1e422dff46a583f82
vshemyako <v.shemyako@gmail.com>
2019-12-15 13:14:47 +0300

    One should interrupt a thread with care

--- src/main/java/practice/ch07/package-info.java
+++ src/main/java/practice/ch07/package-info.java
@@ -6,5 +6,7 @@
  * Calling interrupt does not necessarily stop the target thread from doing what it is doing; it merely delivers the
  * message that interruption has been requested.<br>
  * Interruption is the most sensible way to implement cancellation.<br>
+ * Because each thread has its own interruption policy, you should not interrupt a thread unless you know what
+ * interruption means to that thread.<br>
  **/
 package practice.ch07;
