38ecd08158c6bf40f620b2ffc3470d61786b150b
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-19 22:38:40 +0300

    Be aware of thread starvation deadlock when using bounded executors

---
+++ src/main/java/practice/ch08/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Chapter 8:<br>
+ * <br>
+ * Sub chapter 8.1<br>
+ * Whenever you submit to an Executor tasks which are not independent be aware of thread starvation deadlock.<br>
+ **/
+package practice.ch08;
