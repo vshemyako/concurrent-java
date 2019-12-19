4caf5f37aff99c6a5fb1748b0d0492e59fb81007
Valentine Shemyako <v.shemyako@gmail.com>
2019-12-19 23:23:11 +0300

    Relation between thread pool and pooled resource

--- src/main/java/practice/ch08/package-info.java
+++ src/main/java/practice/ch08/package-info.java
@@ -2,6 +2,8 @@
  * Chapter 8:<br>
  * <br>
  * Sub chapter 8.1<br>
- * Whenever you submit to an Executor tasks which are not independent be aware of thread starvation deadlock.<br>
+ * - Whenever you submit to an Executor tasks which are not independent be aware of thread starvation deadlock.<br>
+ * - In case there's some pooled resource which is required by a task (or vice versa) - size of each is limited by the
+ * maximum size of another.<br>
  **/
 package practice.ch08;
