4a3639d6e5bf7e51bc0ed77ca78dd2be5b9029a2
vshemyako <v.shemyako@gmail.com>
2019-12-20 19:44:43 +0300

    Sequential loop candidates for concurrency

--- src/main/java/practice/ch08/package-info.java
+++ src/main/java/practice/ch08/package-info.java
@@ -5,5 +5,6 @@
  * - Whenever you submit to an Executor tasks which are not independent be aware of thread starvation deadlock.<br>
  * - In case there's some pooled resource which is required by a task (or vice versa) - size of each is limited by the
  * maximum size of another.<br>
+ * - Sequential loop iterations when each iteration is independent of the others are good candidates for parallelization.<br>
  **/
 package practice.ch08;
